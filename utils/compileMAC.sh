16b2da85095a78ec0aac0a34a3b260dbf6c2e238
chihab_eddine98 <ga_benamara@esi.dz>
2021-10-21 10:05:33 +0200

    [DL] bug on installation (too much RAM)

--- utils/compileMAC.sh
+++ utils/compileMAC.sh
@@ -2,4 +2,4 @@
 
 # make sure pybind11 is installed : `pip3 install pybind11`
 
-c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup `python3 -m pybind11 --includes` ../src/golois.cpp -o golois `python3-config --extension-suffix`
+c++ -O3 -Wall -shared -std=c++11 -undefined dynamic_lookup $(python3 -m pybind11 --includes) ../src/golois.cpp -o golois$(python3-config --extension-suffix)
