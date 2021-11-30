744f05bd27ee430293777bbd4d0fce7309125f2d
= <=>
2021-11-30 20:41:41 +0100

    [DMGV2] start shufflnet

--- config/compile.sh
+++ config/compile.sh
@@ -1,1 +1,1 @@
-c++ -O3 -Wall -shared -std=c++11 -fsized-deallocation -fPIC `python3 -m pybind11 --includes` golois/golois.cpp -o golois$(python3-config --extension-suffix)
+c++ -O3 -Wall -shared -std=c++11 -fsized-deallocation -fPIC `python3 -m pybind11 --includes` ../src/golois/golois.cpp -o golois$(python3-config --extension-suffix)
