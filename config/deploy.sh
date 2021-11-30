09010800e37e3bdf0f555faba06b5271cf453b9d
= <=>
2021-11-30 20:41:41 +0100

    [DMGV2] start shufflnet

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,3 +1,5 @@
+# Reservation :
+# oarsub -p "cluster='gemini'" -l host=1,walltime=40 -t deploy -t exotic -r '2021-12-03 20:00:00'
 # This script is to run on top of Debian11-x64
 # Use this to create the deb11 : 
 # kadeploy3 -f $OAR_NODE_FILE -e debian11-x64-base -k
@@ -18,6 +20,7 @@ echo 'Installing Python[3.7.12] stuff...'
 # Python Installation
 apt-get -y build-dep libcurl4-openssl-dev
 apt-get -y install libcurl4-openssl-dev
+apt-get install libffi-dev
 wget https://www.python.org/ftp/python/3.7.12/Python-3.7.12.tgz
 tar -xvf Python-3.7.12.tgz
 cd Python-3.7.12
@@ -31,6 +34,7 @@ make install
 
 echo 'Configuring Repository[DeepGo]...'
 # Repository Installation
+cd /root
 apt install -y git 
 git clone https://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@github.com/ChihabEddine98/DeepGo.git
 cd DeepGo
