0667ad72231e5f310c7a1aa83d82103e042b4f8b
= <=>
2021-12-02 17:55:31 +0100

    Start LR Schudeling pretty print

--- src/train.py
+++ src/train.py
@@ -1,5 +1,10 @@
 import os 
 import tensorflow as tf
+
+from rich.console import Console
+from rich.markdown import Markdown
+
+
 from models.DGV1.model_v1 import DGMV1
 from trainer import Trainer
 from utils import configs
@@ -7,12 +12,12 @@ from utils import configs
 
 if __name__ == '__main__':
 
-    print(f'start training...')
-    
+    console = Console()
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
     strategy = tf.distribute.MirroredStrategy(configs.devices)
 
-    print(f'start training...')
+    title = Markdown(f"# Start training ON {len(configs.devices)}GPU's", style='bold yellow')
+    console.print(title)
 
     # Build the model 
     with strategy.scope():
