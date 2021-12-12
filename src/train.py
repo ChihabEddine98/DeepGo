b5a628875d4e53e022df5907dfaed5c5fe58ad0d
= <ga_benamara@esi.dz>
2021-12-12 23:41:59 +0100

    Command Line Argument Parsing (more pretty code btw 🥳)

--- src/train.py
+++ src/train.py
@@ -1,4 +1,5 @@
 import os 
+import argparse
 import tensorflow as tf
 import tensorflow.keras as keras
 from rich.console import Console
@@ -14,11 +15,54 @@ from trainer import Trainer
 from utils import configs
 
 
+
+
+def console_handler(console):
+    '''
+         ---------------------------------------------------------------
+                    Command Line Guide (args to use)
+         ---------------------------------------------------------------    
+
+        -h    [–-help]          : help with command line 
+        -gpu  [–-num-gpu]       : # of GPUs to be used for training [default : 2]
+        -s    [--start-epoch]   : the epoch to porsuit the training from  [default : 1]
+        -e    [--end-epoch]     : the last epoch of training [default : 500]
+        -b    [--batch-size]    : the batch size used to train model [default : 512]
+        -n    [--num-samples]   : # of samples in the get_batch per epoch [default : 25k]
+        -w    [--show-warnings] : the warnings show level [from 0 to 3]
+    '''
+    # Get All Arguments from Command Line 
+    parser = argparse.ArgumentParser(description='Argument Parser For DGM models.')
+    parser.add_argument('-gpu','--num-gpu',type=int,default=2,metavar='',help="the # of GPUs used to train the model. [default : 2]")
+    parser.add_argument('-s','--start-epoch',type=int,default=1,metavar='',help="the epoch to porsuit the training from  [default : 1]")
+    parser.add_argument('-e','--end-epoch',type=int,default=500,metavar='',help="the last epoch of training [default : 500]")
+    parser.add_argument('-b','--batch-size',type=int,default=512,metavar='',help="the batch size used to train model [default : 512]")
+    parser.add_argument('-n','--num-samples',type=int,default=25_000,metavar='',help="# of samples in the get_batch per epoch [default : 25k]")
+    parser.add_argument('-w','--show-warnings',type=int,default=2,metavar='',help="the warnings show level [from 0 to 3]")
+    args = parser.parse_args()
+    
+    # Set Parsed Parameters into configs
+    configs.devices = [f"/device:GPU:{i}" for i in range(args.num_gpu)]
+    configs.start_epoch = args.start_epoch
+    configs.end_epoch = args.end_epoch
+    configs.batch_size = args.batch_size
+    configs.n_samples = args.num_samples
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(args.show_warnings)
+
+
+    console.print(f" Here are your `configs` : {configs}")
+
+    # Make it Parallel 🥳
+    strategy = tf.distribute.MirroredStrategy(configs.devices)
+    return strategy
+
+
+
+
 if __name__ == '__main__':
 
     console = Console()
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-    strategy = tf.distribute.MirroredStrategy(configs.devices)
+    strategy = console_handler(console)
 
     title = Markdown(f"# Start training ON {len(configs.devices)} GPU's", style=configs.info_style)
     console.print(title)
