72831ef7dea48c370e53d08282571b61521d9b97
root <ga_benamara@esi.dz>
2021-12-13 17:17:04 +0100

    [MOBNET+] 256-64-17-5-swish : 200 epochs : acc : [44.44%] mse : [0.077] ✅

--- src/models/DGV6/model_v6.py
+++ src/models/DGV6/model_v6.py
@@ -13,10 +13,10 @@ from models.DGV0.model_v0 import DGM
 
 config = DotDict({  'n_filters'     : 256,
                     'squeeze'       : 64,
-                    'kernel'        : 3,
+                    'kernel'        : 5,
                     'pool'          : 3 ,
                     'n_res_blocks'  : 6,
-                    'n_btk_blocks'  : 8,
+                    'n_btk_blocks'  : 17,
                     'l2_reg'        : 0.0001,
                     'dropout'       : 0.2,
                     'repetitions'   : (3,7,3),
@@ -51,66 +51,57 @@ class DGMV6(DGM):
     def build_model(self,n_blocks=config.n_btk_blocks):
         return super().build_model(n_blocks)
     
-
-
-    def input_block(self,x):
-        x = self.conv_bn(x, filters=self.n_filters//8, kernel_size=self.kernel, strides=2)
-        x = self.activation(x)
-        x = self.conv_bn(x, filters=self.n_filters//4, kernel_size=self.kernel)
-        tensor = self.activation(x)
-
-    
-        x = self.sep_bn(tensor, filters=self.n_filters//2, kernel_size=self.kernel)
-        x = self.activation(x)
-
-        x = self.sep_bn(x, filters=self.n_filters//2, kernel_size=self.kernel)
-        x = layers.MaxPool2D(pool_size=self.pool, strides=2, padding='same')(x)
-    
-        tensor = self.conv_bn(tensor, filters=self.n_filters//2, kernel_size=1, strides=2)
-    
-        x = layers.add([tensor, x])
-        x = self.activation(x)
-        x = self.sep_bn(x, filters=self.n_filters, kernel_size=self.kernel)
-        x = self.activation(x)
-
-        x = self.sep_bn(x,filters=self.n_filters, kernel_size=self.kernel)
-        x = layers.MaxPool2D(pool_size=self.pool, strides=2, padding='same')(x)
-    
-        tensor = self.conv_bn(tensor, filters=self.n_filters, kernel_size=1, strides=2)
-    
-        x = layers.add([tensor, x])
-    
+    def body_block(self,x,n_blocks=config.n_btk_blocks):
+        
+        for _ in range(n_blocks):
+            x = self.bottleneck_block(x)
         return x
 
-    def residual_block(self, tensor, pad='same'):
+    def bottleneck_block(self,x, expand=config.n_filters, squeeze=config.squeeze):
+        m = layers.Conv2D(expand, 1,kernel_regularizer=self.l2_reg,use_bias = 0)(x)
+        m = layers.BatchNormalization()(m)
+        m = self.activation(m)
+        m = layers.DepthwiseConv2D(self.kernel, padding='same',kernel_regularizer=self.l2_reg,use_bias = 0)(m)
+        m = layers.BatchNormalization()(m)
+        m = self.activation(m)
 
-        x = self.activation(tensor)
-        x = self.sep_bn(x, filters=self.n_filters, kernel_size=self.kernel)
+        m = self.sub_residual_block(m,ratio=16)
+
+        m = layers.Conv2D(squeeze, 1,kernel_regularizer=self.l2_reg,use_bias = 0)(m)
+        m = layers.BatchNormalization()(m)
+
+        return layers.add([m, x])
+
+    def input_block(self,inp,kernel_resize=7,pad='same'):
+        # CONV2D + BN + activation 
+        x = layers.Conv2D(self.squeeze, 5, padding=pad)(inp)
+        x = layers.BatchNormalization()(x)
         x = self.activation(x)
-        x = self.sep_bn(x, filters=self.n_filters, kernel_size=self.kernel)
-        x = self.activation(x)
-        x = self.sep_bn(x, filters=self.n_filters, kernel_size=self.kernel)
- 
-        tensor = layers.add([tensor, x])
- 
-        return tensor
 
-
-    def conv_bn(self,x, filters, kernel_size, strides=1):
-        x = layers.Conv2D(filters=filters,
-                          kernel_size=kernel_size,
-                          strides=strides,
-                          padding='same',
-                          use_bias=False)(x)
-        return layers.BatchNormalization()(x)
+        
+        # CONV2D (resize) + BN + activation
+        x1 = layers.Conv2D(self.squeeze,kernel_resize, padding=pad)(inp)
+        x1 = layers.BatchNormalization()(x1)
+        x1 = self.activation(x1)
+        x = layers.add([x, x1])
+        
+        return x
     
-    def sep_bn(self,x, filters, kernel_size, strides=1):
-        x = layers.SeparableConv2D(filters=filters,
-                            kernel_size=kernel_size,
-                            strides=strides,
-                            padding='same',
-                            use_bias=False)(x)
-        return layers.BatchNormalization()(x)
+    def sub_residual_block(self,x1,ratio=16):
+        x = layers.Dropout(self.dropout)(x1)
+        x = layers.GlobalMaxPooling2D()(x)
+        x = layers.Dense(self.n_filters//ratio, activation='relu')(x)
+        x = layers.Dense(self.n_filters, activation='sigmoid')(x)
+        return layers.Multiply()([x1, x])
+
+    def output_value_block(self,x):
+        value_head = layers.GlobalAveragePooling2D()(x)
+        value_head = layers.Dense(self.squeeze, kernel_regularizer=self.l2_reg)(value_head)
+        value_head = layers.BatchNormalization()(value_head)
+        value_head = self.activation(value_head)
+        value_head = layers.Dropout(self.dropout)(value_head)
+        value_head = layers.Dense(1, activation='sigmoid', name='value', kernel_regularizer=self.l2_reg)(value_head)
+        return value_head
 
     def activation(self, x):
-        return nn.swish(x) 
+        return nn.swish(x)
