b242ae44640cd10c5407b4f5c0b70f21d5f81667
root <ga_benamara@esi.dz>
2021-12-21 16:39:32 +0100

    MNET - 19 blocks - 0.00125 - 3000 epochs -- 80/3000 -- 42% for now

--- src/trainer.py
+++ src/trainer.py
@@ -114,8 +114,8 @@ class Trainer(object):
                 console.print(title)
                 val_hist.append(val)
                 histories['val_history'] = val_hist
-                self.model.save(f"mnas_256_1.0.0005_sw_{self.config.start_epoch}_to_{self.config.end_epoch}_{self.model_path}")                  
-                with open(f"{self.hist_path}_mnas_256_1.0.0005_sw_{self.config.end_epoch}",'wb') as f_hist:
+                self.model.save(f"mnet_256_19_125_sw_{self.config.start_epoch}_to_{self.config.end_epoch}_{self.model_path}")                  
+                with open(f"{self.hist_path}_mnet_256_19_125_sw_{self.config.end_epoch}",'wb') as f_hist:
                      pickle.dump(histories,f_hist)
                     
         title = Markdown(f"## END of Training Saving Last [DGM]...", style=self.config.succes_style)
